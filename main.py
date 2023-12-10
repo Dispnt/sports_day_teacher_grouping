b05fc3015d7a6c4724e386b0749652bf879f745e
dispnt <yuyuanjiajian@gmail.com>
2023-12-10 18:53:11 +0800

    add event signup

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/TeacherSports'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost/TeacherSports'
 db = SQLAlchemy(app)
 
 class VotingRecords(db.Model):
@@ -13,10 +13,33 @@ class VotingRecords(db.Model):
     team = db.Column(db.String(10), nullable=False)
     timestamp = db.Column(db.TIMESTAMP, server_default=db.func.now(), nullable=False)
 
+class TeacherProjects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    teacher_name = db.Column(db.String(255), nullable=False)
+    project_name = db.Column(db.String(255), nullable=False)
+    team = db.Column(db.String(10), nullable=False)
+    timestamp = db.Column(db.TIMESTAMP, server_default=db.func.now(), nullable=False)
+
+# 映射英文项目名称到中文
+english_to_chinese_map = {
+    'Solid Ball': '实心球',
+    'Basketball': '篮球',
+    'Badminton': '羽毛球',
+    'Jump Rope': '跳绳',
+    'Blue': '蓝',
+    'Red': '红'
+    # 添加其他项目的映射
+}
+
+# 根据英文项目名称返回中文名称的方法
+def translate_to_chinese(english_name):
+    return english_to_chinese_map.get(english_name, english_name) 
+
+
 # 首页路由，渲染投票页面
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('eventvote.html')
 
 # 投票接口路由
 @app.route('/vote', methods=['POST'])
@@ -63,6 +86,75 @@ def get_vote_status():
 
     except Exception as e:
         return jsonify({'success': False, 'message': str(e)})
+
+
+@app.route('/vote/project', methods=['POST'])
+def vote_project():
+    try:
+        data = request.json
+        name = data.get('name')
+        project = data.get('project')
+        team = data.get('team')
+
+        # Check if the teacher has already signed up for this project
+        existing_vote = TeacherProjects.query.filter_by(teacher_name=name, project_name=project, team=team).first()
+
+        if existing_vote:
+            # If the teacher has already signed up, cancel the registration
+            db.session.delete(existing_vote)
+            db.session.commit()
+            return jsonify({'success': True, 'message': f'取消报名 {translate_to_chinese(project)} 项目成功！'})
+
+        # Check if the teacher can participate in the project
+        if TeacherProjects.query.filter_by(teacher_name=name, team=team).count() >= 3:
+            return jsonify({'success': False, 'message': '您最多可以选择三个项目。'})
+
+        # Check if the team is full
+        team_size = TeacherProjects.query.filter_by(team=team, project_name=project).count()
+        if team_size >= 5:
+            return jsonify({'success': False, 'message': f'{translate_to_chinese(team)}队的 {translate_to_chinese(project)} 项目已满，不能再选。'})
+
+        # Insert a new record
+        new_vote_project = TeacherProjects(teacher_name=name, project_name=project, team=team)
+        db.session.add(new_vote_project)
+        db.session.commit()
+
+        return jsonify({'success': True, 'message': '选择项目成功！'})
+
+    except Exception as e:
+        return jsonify({'success': False, 'message': str(e)})
+
+@app.route('/project/team/count', methods=['GET'])
+def get_project_team_count():
+    try:
+        project = request.args.get('project')
+        team = request.args.get('team')
+
+        count = TeacherProjects.query.filter_by(project_name=project, team=team).count()
+
+        return jsonify({'success': True, 'count': count})
+
+    except Exception as e:
+        return jsonify({'success': False, 'message': str(e)})
+
+
+
+@app.route('/project/enrolled', methods=['GET'])
+def get_enrolled_projects():
+    try:
+        name = request.args.get('name')  # 通过查询参数获取教工姓名
+        team = request.args.get('team')  # 通过查询参数获取队伍名称
+        print(name,team)
+        # 查询当前报名了哪些项目
+        enrolled_projects = TeacherProjects.query.filter_by(teacher_name=name, team=team).all()
+        # 构造项目列表
+        projects_list = [{'project': project.project_name, 'timestamp': project.timestamp} for project in enrolled_projects]
+
+        return jsonify({'success': True, 'projects': projects_list})
+
+    except Exception as e:
+        return jsonify({'success': False, 'message': str(e)})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
--- sql.sql
+++ sql.sql
@@ -10,3 +10,12 @@ CREATE TABLE IF NOT EXISTS voting_records (
 INSERT INTO voting_records (teacher_name, team) VALUES
     ('张伟', 'blue'),
     ('张军', 'red');
+	
+	
+CREATE TABLE IF NOT EXISTS teacher_projects (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	teacher_name VARCHAR(255) NOT NULL,
+	project_name VARCHAR(255) NOT NULL,
+	team VARCHAR(10) NOT NULL,
+	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
